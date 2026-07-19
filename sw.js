7283c070f19418375f2c8b9c34ec08c1e7af3858
ekarum <eyupkarum@gmail.com>
2026-07-19 18:19:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='karum-toto-v7-1-1';
+const CACHE='karum-toto-v7.1.1';
 const ASSETS=['./','./index.html','./styles.css?v=7.1','./app.js?v=7.1','./initial-data.js?v=7.1','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
