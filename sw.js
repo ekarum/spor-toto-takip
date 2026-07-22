0134826d17771f9291eab43bc0f0217623f6c1e1
ekarum <eyupkarum@gmail.com>
2026-07-22 16:19:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='karum-toto-v7.3.2';
-const ASSETS=['./','./index.html','./styles.css?v=7.3.2','./app.js?v=7.3.2','./initial-data.js?v=7.3.2','./manifest.webmanifest','./karum-logo-64.png?v=7.3.2','./karum-logo-192.png?v=7.3.2','./karum-logo-512.png?v=7.3.2'];
+const CACHE='karum-toto-v7.3.3';
+const ASSETS=['./','./index.html','./styles.css?v=7.3.3','./app.js?v=7.3.3','./initial-data.js?v=7.3.3','./manifest.webmanifest','./karum-logo-64.png?v=7.3.3','./karum-logo-192.png?v=7.3.3','./karum-logo-512.png?v=7.3.3','./karum-logo-symbol.png?v=7.3.3','./karum-logo-full.png?v=7.3.3'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
