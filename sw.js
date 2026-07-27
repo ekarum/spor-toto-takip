0b5f6affdd80468c2ea3c0d53ce26fdd4508130a
ekarum <eyupkarum@gmail.com>
2026-07-27 16:21:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_VERSION='karum-toto-v20.3.1';
+const CACHE_VERSION='karum-toto-v20.3.2';
 const APP_SHELL=[
   './',
   './index.html',
-  './styles.css?v=20.3.1',
-  './app.js?v=20.3.1',
-  './initial-data.js?v=20.3.1',
-  './manifest.webmanifest?v=20.3.1',
-  './apple-touch-icon.png?v=20.3.1',
+  './styles.css?v=20.3.2',
+  './app.js?v=20.3.2',
+  './initial-data.js?v=20.3.2',
+  './manifest.webmanifest?v=20.3.2',
+  './apple-touch-icon.png?v=20.3.2',
   './karum-logo-192.png',
   './karum-logo-512.png'
 ];
